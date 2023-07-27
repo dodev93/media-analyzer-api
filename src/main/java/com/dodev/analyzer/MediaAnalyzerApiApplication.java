449eb9220a7e01b1d838b744b6fa778b909b922b
bobo <danioch835@gmail.com>
2023-07-27 22:39:29 +0200

    Initial commit

---
+++ src/main/java/com/dodev/analyzer/MediaAnalyzerApiApplication.java
@@ -0,0 +1,12 @@
+package com.dodev.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaAnalyzerApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MediaAnalyzerApiApplication.class, args);
+	}
+
+}
